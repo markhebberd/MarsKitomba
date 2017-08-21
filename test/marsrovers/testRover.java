2d5817dbcfbce6afc0e697f6bd624ab16e5cd560
markhebberd <markhebberd@gmail.com>
2017-08-21 13:12:17 +1200

    Refactored to include NasaControlCenter to manage rover movement.

--- test/marsrovers/testRover.java
+++ test/marsrovers/testRover.java
@@ -7,18 +7,17 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
-import org.junit.Rule;
-import org.junit.rules.ExpectedException;
 
 /**
  *
- * @author markh
+ * @author markhebberd@gmail.com
  */
 public class testRover {
     int[] gridSize= {5,5};
-    PlatauGrid pg = new PlatauGrid( gridSize );
+    PlateauGrid pg = new PlateauGrid( gridSize );
     RoverPosition rp = new RoverPosition(0,0,Direction.N);
     Rover rover = new Rover(rp);
+    NasaControlCenter nasaControlCenter = new NasaControlCenter(pg);
     
     public testRover() {
     }
@@ -33,7 +32,7 @@ public class testRover {
     
     @Before
     public void setUp() {
-        pg.setRover(rover);
+        nasaControlCenter.setRover(rover);
     }
     
     @After
@@ -42,35 +41,36 @@ public class testRover {
         pg = null;
         rp = null;
         rover = null;
+        nasaControlCenter = null;
     }
 
     @Test
     public void testRoverTurnRight() throws OutOfBoundsException {         
-        pg.moveRover("R", false);
+        nasaControlCenter.moveRover("R", false);
         assertEquals(rover.position.getDirection(), Direction.E);
     }
     
     @Test
     public void testRoverTurnLeft() throws OutOfBoundsException { 
-        pg.moveRover("L", false);
+        nasaControlCenter.moveRover("L", false);
         assertEquals(rover.position.getDirection(), Direction.W);
     }
     
     @Test
     public void testRoverTurnRight360() throws OutOfBoundsException { 
-        pg.moveRover("RRRR", false);
+        nasaControlCenter.moveRover("RRRR", false);
         assertEquals(rover.position.getDirection(), Direction.N);
     }
     
     @Test
     public void testRoverTurnLeft360() throws OutOfBoundsException { 
-        pg.moveRover("LLLL", false);
+        nasaControlCenter.moveRover("LLLL", false);
         assertEquals(rover.position.getDirection(), Direction.N);
     }
     
     @Test
     public void testRoverTurnRightatEachCorner() throws OutOfBoundsException { 
-        pg.moveRover("MMMMMRMMMMMRMMMMMRMMMMM", false);
+        nasaControlCenter.moveRover("MMMMMRMMMMMRMMMMMRMMMMM", false);
         assertEquals(rover.position.getDirection(), Direction.W);
         assertEquals(rover.position.getxPosition(), 0);
         assertEquals(rover.position.getyPosition(), 0);
@@ -78,22 +78,22 @@ public class testRover {
     
     @Test(expected=OutOfBoundsException.class)
     public void testOutOfBoundsExceptionWest() throws OutOfBoundsException { 
-        pg.moveRover("LM", false);
+        nasaControlCenter.moveRover("LM", false);
     }
     
     @Test(expected=OutOfBoundsException.class)
     public void testOutOfBoundsExceptionNorth() throws OutOfBoundsException { 
-        pg.moveRover("MMMMMM", false);
+        nasaControlCenter.moveRover("MMMMMM", false);
     }
     
     @Test(expected=OutOfBoundsException.class)
     public void testOutOfBoundsExceptionSouth() throws OutOfBoundsException { 
-        pg.moveRover("LLM", false);
+        nasaControlCenter.moveRover("LLM", false);
     }
     
     @Test(expected=OutOfBoundsException.class)
     public void testOutOfBoundsExceptionEast() throws OutOfBoundsException { 
-        pg.moveRover("RMMMMMM", false);
+        nasaControlCenter.moveRover("RMMMMMM", false);
     }
     
 }
